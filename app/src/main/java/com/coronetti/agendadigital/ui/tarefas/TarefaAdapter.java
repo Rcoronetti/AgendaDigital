89fc4215320d6da734c051de0a749dc09f97cd42
Rafael Coronetti <rafaelcoronetti@gmail.com>
2024-10-05 10:06:53 -0300

    Melhora a tela de acompanhamento de tarefas inserindo botões para edição e exclusão das tarefas

--- app/src/main/java/com/coronetti/agendadigital/ui/tarefas/TarefaAdapter.java
+++ app/src/main/java/com/coronetti/agendadigital/ui/tarefas/TarefaAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.coronetti.agendadigital.R;
@@ -23,7 +24,6 @@ public class TarefaAdapter extends ArrayAdapter<Tarefa> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Verifica se a view já foi criada
         if (convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(R.layout.fragment_item_tarefa, parent, false);
@@ -32,12 +32,14 @@ public class TarefaAdapter extends ArrayAdapter<Tarefa> {
         // Obtém a tarefa atual
         Tarefa tarefa = tarefas.get(position);
 
-        // Configura os elementos do item_tarefa.xml
+        // Configura os elementos
         TextView textTitulo = convertView.findViewById(R.id.textTitulo);
         TextView textTurma = convertView.findViewById(R.id.textTurma);
         TextView textData = convertView.findViewById(R.id.textData);
         TextView textDisciplina = convertView.findViewById(R.id.textDisciplina);
         TextView textStatus = convertView.findViewById(R.id.textStatus);
+        Button buttonStatus = convertView.findViewById(R.id.buttonStatus);
+        Button buttonExcluir = convertView.findViewById(R.id.buttonExcluir);
 
         // Define os valores para os TextViews
         textTitulo.setText(tarefa.getTitulo());
@@ -46,6 +48,25 @@ public class TarefaAdapter extends ArrayAdapter<Tarefa> {
         textDisciplina.setText(tarefa.getDisciplina());
         textStatus.setText(tarefa.getStatus());
 
+        // Ação para editar o status
+        buttonStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tarefa.setStatus("Concluída");
+                notifyDataSetChanged(); // Atualiza a lista
+            }
+        });
+
+        // Ação para excluir a tarefa
+        buttonExcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tarefas.remove(position);
+                notifyDataSetChanged(); // Atualiza a lista
+            }
+        });
+
         return convertView;
     }
+
 }
